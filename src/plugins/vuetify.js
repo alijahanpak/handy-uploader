61a9c1255a64578db5c4ce300a30462e8418f4ba
ali jahanpak <a.jahnpak@gmail.com>
2021-01-02 09:09:26 +0330

    improve ui 2021-01-02

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,11 @@
-import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    icons: {
-        iconfont: 'mdi', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
-    },
+  icons: {
+    iconfont: "mdi" // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+  }
 });
