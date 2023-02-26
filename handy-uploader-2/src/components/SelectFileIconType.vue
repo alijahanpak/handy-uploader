6fc26e9eb2c024e45f3fb9d2c785b3beacf99935
ali jahanpak <a.jahanpak@gmail.com>
2023-02-26 22:55:37 +0100

    Create vite app

---
+++ handy-uploader-2/src/components/SelectFileIconType.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <v-icon
+      v-if="format === 'pdf'"
+      :size="size"
+      file-word-outline
+      color="red darken-1"
+      >mdi-file-pdf-outline</v-icon
+    >
+    <v-icon
+      v-else-if="
+        fileFormat === 'doc' || fileFormat === 'docx' || fileFormat === 'odt'
+      "
+      :size="size"
+      file-word-outline
+      color="blue darken-1"
+      >mdi-file-word-outline</v-icon
+    >
+    <v-icon
+      v-else-if="
+        fileFormat === 'jpg' ||
+          fileFormat === 'jpeg' ||
+          fileFormat === 'png' ||
+          fileFormat === 'tif' ||
+          fileFormat === 'bmp'
+      "
+      :size="size"
+      file-word-outline
+      color="deep-purple darken-1"
+      >mdi-file-image-outline</v-icon
+    >
+    <v-icon
+      v-else-if="fileFormat === 'xls' || fileFormat === 'xlsx'"
+      :size="size"
+      file-word-outline
+      color="teal darken-1"
+      >mdi-file-excel-outline</v-icon
+    >
+    <v-icon
+      v-else-if="
+        fileFormat === 'pptx' || fileFormat === 'pptm' || fileFormat === 'ppt'
+      "
+      :size="size"
+      file-word-outline
+      color="orange darken-3"
+      >mdi-file-powerpoint-outline</v-icon
+    >
+    <v-icon
+      v-else-if="
+        fileFormat === 'mp4' ||
+          fileFormat === 'mov' ||
+          fileFormat === 'flv' ||
+          fileFormat === 'wmv' ||
+          fileFormat === 'avi'
+      "
+      :size="size"
+      file-word-outline
+      color="red lighten-1"
+      >mdi-file-video-outline</v-icon
+    >
+    <v-icon
+      v-else-if="fileFormat === 'dwg'"
+      :size="size"
+      file-word-outline
+      color="indigo lighten-2"
+      >mdi-file-cad</v-icon
+    >
+    <v-icon
+      v-else-if="
+        fileFormat === 'zip' || fileFormat === 'rar' || fileFormat === '7-zip'
+      "
+      :size="size"
+      file-word-outline
+      color="lime lighten-1"
+      >mdi-folder-zip-outline</v-icon
+    >
+    <v-icon
+      v-else-if="fileFormat === 'txt'"
+      :size="size"
+      file-word-outline
+      color="light-green darken-3"
+      >mdi-script-text-outline</v-icon
+    >
+    <v-icon v-else :size="size" file-word-outline color="indigo lighten-1"
+      >mdi-file-question-outline</v-icon
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    fileFormat: {
+      type: String,
+      default: ""
+    },
+    size: {
+      type: Number,
+      default: 30
+    }
+  }
+};
+</script>
---
+++ handy-uploader-2/src/components/language.js
@@ -0,0 +1,148 @@
+export default {
+  en: {
+    insertFile: "Insert File",
+    insertNewFile: "Insert New File",
+    add: "Add",
+    delete: "Delete",
+    edit: "Edit",
+    deleteDialog: {
+      message: "Are you sure you want to delete the file?",
+      cancel: "cancel"
+    },
+    table: {
+      thumb: "Thumb",
+      name: "Name",
+      size: "Size",
+      tags: "tags",
+      action: {
+        action: "Action",
+        deleteTooltip: "Delete"
+      }
+    },
+    size: {
+      kb: "KB",
+      mb: "MB"
+    },
+    maxFileSizeAlert: "Max file Size is",
+    maxFileCountAlert: "Max file Count is",
+    fileName: "File Name",
+    fileDescription: "File Description",
+    fileTags: "File Tags"
+  },
+  fa: {
+    insertFile: "افزودن فایل",
+    insertNewFile: "افزودن فایل جدید",
+    add: "افزودن",
+    delete: "حذف",
+    edit: "ویرایش",
+    deleteDialog: {
+      message: "آیا برای حذف فایل اطمینان دارید؟",
+      cancel: "لغو"
+    },
+    table: {
+      thumb: "پیش نمایش",
+      name: "نام",
+      size: "حجم",
+      action: {
+        action: "عملیات",
+        deleteTooltip: "حذف"
+      }
+    },
+    size: {
+      kb: "کیلو بایت",
+      mb: "مگابایت"
+    },
+    maxSizeAlert: "حداکثر حجم فایل انتحابی ",
+    maxFileCountAlert: "حداکثر تعداد فایل انتخابی",
+    fileName: "نام فایل",
+    fileDescription: "توضیحات فایل",
+    fileTags: "برچسب فایل"
+  },
+  fr: {
+    insertFile: "Insérer un fichier",
+    insertNewFile: "Insérer un nouveau fichier",
+    add: "Ajouter",
+    delete: "Supprimer",
+    edit: "Éditer",
+    deleteDialog: {
+      message: "Voulez-vous vraiment supprimer le fichier?",
+      cancel: "Annuler"
+    },
+    table: {
+      thumb: "la vignette",
+      name: "Nom",
+      size: "Taille",
+      action: {
+        action: "Action",
+        deleteTooltip: "Supprimer"
+      }
+    },
+    size: {
+      kb: "KB",
+      mb: "MB"
+    },
+    maxSizeAlert: "La taille maximale du fichier est",
+    maxFileCountAlert: "Le nombre maximal de fichiers est",
+    fileName: "Nom de fichier",
+    fileDescription: "description du fichier",
+    fileTags: "Balises de fichier"
+  },
+  ch: {
+    insertFile: "插入档案",
+    insertNewFile: "插入新文件",
+    add: "加",
+    delete: "删除",
+    edit: "编辑",
+    deleteDialog: {
+      message: "您确定要删除文件吗?",
+      cancel: "取消"
+    },
+    table: {
+      thumb: "缩图",
+      name: "名称",
+      size: "尺寸",
+      action: {
+        action: "行动",
+        deleteTooltip: "删除"
+      }
+    },
+    size: {
+      kb: "KB",
+      mb: "MB"
+    },
+    maxSizeAlert: "档案大小上限为",
+    maxFileCountAlert: "现“最大文件数”为",
+    fileName: "文档名称",
+    fileDescription: "文件描述",
+    fileTags: "文件标签"
+  },
+  ar: {
+    insertFile: "إدراج ملف",
+    insertNewFile: "إدراج ملف جديد",
+    add: "أضف",
+    delete: "حذف",
+    edit: "تعديل",
+    deleteDialog: {
+      message: "هل أنت متأكد أنك تريد حذف الملف؟",
+      cancel: "إلغاء"
+    },
+    table: {
+      thumb: "ظفري",
+      name: "اسم",
+      size: "بحجم",
+      action: {
+        action: "عمل",
+        deleteTooltip: "حذف"
+      }
+    },
+    size: {
+      kb: "کیلو بایت",
+      mb: "ميغا بايت"
+    },
+    maxSizeAlert: "الحجم الأقصى للملف هو",
+    maxFileCountAlert: "الحد الأقصى لعدد الملفات هو",
+    fileName: "اسم الملف",
+    fileDescription: "وصف الملف",
+    fileTags: "علامات الملف"
+  }
+};
